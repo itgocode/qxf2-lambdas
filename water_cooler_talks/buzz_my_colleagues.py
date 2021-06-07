720ae7d6e4661b03c7523052d2038acd6c017471
PreedhiVivek <preedhi.vivek@qxf2.com>
2021-06-07 12:55:36 +0530

    Refactored code.

--- water_cooler_talks/buzz_my_colleagues.py
+++ water_cooler_talks/buzz_my_colleagues.py
@@ -1,6 +1,6 @@
 """
 Lambda to
-    buzz colleagues on their raspberryPi device,
+    buzz colleagues on their Raspberry Pi device,
     when a colleague wants to have water cooler conversations.
 """
 import os
@@ -16,23 +16,28 @@ def get_participants_count():
         if response.status_code == 200:
             participants_count = response.json()['participants']
         elif response.status_code == 404:
-            print('No participants in room')
+            print('\n No participants in room!')
     except requests.exceptions.RequestException as request_error:
-        print(f'Request error: \n {request_error}')
+        print(f'\n Request error: \n {request_error}')
         raise Exception ('Error while fetching info from Jitsi server!') from request_error
     except Exception as error:
-        print(f'Error encountered: \n {error}')
+        print(f'\n Error encountered: \n {error}')
         raise Exception('Error while fetching info from Jitsi server!') from error
     return participants_count
 
-def lambda_handler(event, context):
-    "Lambda entry point."
+def buzz_colleagues(participants_count):
+    """
+    Buzz colleagues based on the status of buzz_flag, when the room has participants.
+    """
     buzz_flag = False # Line should be removed and variable's state should be stored in AWS
-    peers_count = get_participants_count()
-    if peers_count > 0:
+    if participants_count > 0:
         if buzz_flag is False:
-            # Add the code that integrates with raspberryPi to buzz colleagues here.
+            # Add the code that integrates with Raspberry Pi to buzz colleagues here.
             print('Buzzing colleagues to chit-chat!')
             buzz_flag = True
     else:
         buzz_flag = False
+
+def lambda_handler(event, context):
+    "Lambda entry point."
+    buzz_colleagues(get_participants_count())
