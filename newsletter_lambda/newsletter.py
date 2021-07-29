9a6ed5765ec16b1b4893dfd1cfa654ec273550e2
Drishya <drishya.tm@qxf2.com>
2021-07-29 22:19:43 +0530

    added url extraction

--- newsletter_lambda/newsletter.py
+++ newsletter_lambda/newsletter.py
@@ -6,9 +6,17 @@ import json
 import os
 import boto3
 import requests
+from urlextract import URLExtract
 #IS_PTO_URL = 'https://practice-testing-ai-ml.qxf2.com/is-pto'
 QUEUE_URL = 'https://sqs.ap-south-1.amazonaws.com/285993504765/skype-sender'
 
+def get_url_from_message(message):
+        extractor = URLExtract()
+        urls = extractor.find_urls(message)
+        
+        #url_extract = re.findall(r'(https?://\S+)', message)
+        print("URL on extract",urls[0])
+        return (urls[0])
 
 def clean_message(message):
     "Clean up the message received"
@@ -21,6 +29,7 @@ def clean_message(message):
 
 def get_message_contents(event):
     "Retrieve the message contents from the SQS event"
+    print("Empty message")
     record = event.get('Records')[0]
     message = record.get('body')
     message = json.loads(message)['Message']
@@ -30,19 +39,19 @@ def get_message_contents(event):
     return message
 
 def lambda_handler(event, context):
-
     "Lambda entry point"
+    
     message_contents = get_message_contents(event)
     message = message_contents['msg']
     channel = message_contents['chat_id']
     user = message_contents['user_id']
     print(f'{message}, {user}, {channel}')
     is_pto_flag = False
-    if channel == os.environ.get('ETC_CHANNEL') and user != os.environ.get('Qxf2Bot_USER'):
-        cleaned_message = clean_message(message)
-        #is_pto_flag = get_is_pto(cleaned_message)
+    if channel == os.environ.get('ETC_CHANNEL'):
         print("Inside the Channel")
-   
+        cleaned_message = clean_message(message)
+        url = get_url_from_message(message)
+        print ("one url only",url)
 
 
 
