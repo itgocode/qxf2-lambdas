8b4d16c342cb2e06b472400e50f839fc915ba1f5
Raghava <raghava.nelabhotla@qxf2.com>
2021-08-05 13:14:39 +0530

    added method to write messages to sqs queue

--- url_filter_rag/url_filter_rag.py
+++ url_filter_rag/url_filter_rag.py
@@ -9,6 +9,8 @@ import requests
 import re
 from requests import Response
 
+QUEUE_URL = 'https://sqs.ap-south-1.amazonaws.com/285993504765/rag-queue'
+
 #create lambda client
 def clean_message(message):
     "Clean up the message received"
@@ -30,13 +32,19 @@ def get_message_contents(event):
     "Retrieve the message contents from the SQS event"
     record = event.get('Records')[0]
     message = record.get('body')
-    message = json.loads(message)['Message']
+    message = json.loads(message)["Message"]
     message = json.loads(message)
 
     return message
 
+def write_message(message, channel):
+    "Send a message to Skype Sender"
+    sqs = boto3.client('sqs')
+    message = str({'msg':f'{message}', 'channel':channel})
+    sqs.send_message(QueueUrl=QUEUE_URL, MessageBody=(message))
+
 def post_newsletter_url(json_url):
-    URL = https://newsletter-generator.qxf2.com/articles
+    URL = "https://newsletter-generator.qxf2.com/articles"
     data = json.dumps(json_url)
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     response = requests.post(URL, data=data, headers=headers )
@@ -54,8 +62,12 @@ def lambda_handler(event, context):
     if channel == os.environ.get('ETC_CHANNEL') and user != os.environ.get('Qxf2Bot_USER'):
         cleaned_message = clean_message(message)
         final_url = URL_filter(cleaned_message)
-        if final_url:
+
+        if final_url and user != os.environ.get('Qxf2Bot_USER'):
+            message_to_send = f'Detected URL {cleaned_message}'
+            write_message(message_to_send, os.environ.get('SEND_CHANNEL'))
             response = post_newsletter_url(final_url)
             assert response.status_code == 200
     else:
         print("message not found in etc channel")
+
